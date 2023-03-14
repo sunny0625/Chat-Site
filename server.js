89bded170e2280611a91cc2151704cd37122229c
Sukhbir Singh <sukhbir2506@outlook.com>
2023-03-14 23:02:54 +0530

    Node

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const server = require("http").createServer(app);
 
 const io = require('socket.io')(server);
-app.use(express.static(path.join(__dirname + '/public/')));
+app.use(express.static(path.join(__dirname + '/')));
 
 io.on("connection", function(socket) {
     socket.on("newuser", function(username) {
